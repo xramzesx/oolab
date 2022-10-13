8620b0713e6ee9820a766ff66973e9b3d1ce969e
xramzesx <xramzesbusiness@gmail.com>
2022-10-13 13:42:37 +0200

    Initial commit

---
+++ src/main/java/agh/ics/oop/Direction.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+
+public enum Direction {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT,
+    NONE;
+
+    public String toString() {
+        return switch (this) {
+            case FORWARD -> "f";
+            case BACKWARD -> "b";
+            case RIGHT -> "r";
+            case LEFT -> "l";
+            default -> null;
+        };
+    }
+
+    public static Direction toEnum( String direction ) {
+        return switch (direction) {
+            case "f" -> FORWARD;
+            case "b" -> BACKWARD;
+            case "r" -> RIGHT;
+            case "l" -> LEFT;
+            default -> NONE;
+        };
+    }
+}
---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,72 @@
+package agh.ics.oop;
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+public class World {
+
+    public static void main ( String[] args ) {
+        Direction[] requests = convertToDirections(args);
+        out.println("system wystartowal");
+
+        printLine();
+        run(requests);
+
+        printLine();
+        out.println("Komunikaty: " + String.join(
+                ",",
+                Arrays
+                        .stream(args)
+                        .filter(
+                                direction -> Direction.toEnum(direction) != Direction.NONE
+                        )
+                        .toArray(String[]::new)
+        ));
+
+        printLine();
+        out.println("system zakonczyl dzialanie");
+    }
+
+    private static void run (Direction[] requests) {
+        // zad8: tu drobna modyfikacja, żeby nie wpływała
+        // na dalszą część programu/wyświetlanego tekstu
+        out.println("zwierzak wystartowal");
+
+        for (Direction request : requests ) {
+            switch (request) {
+                case FORWARD:
+                    out.println("Zwierzak idzie do przodu");
+                    break;
+                case BACKWARD:
+                    out.println("Zwierzak idzie do tyłu");
+                    break;
+                case RIGHT:
+                    out.println("Zwierzak skręca w prawo");
+                    break;
+                case LEFT:
+                    out.println("Zwierzak skręca w lewo");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        out.println("zwierzak zastopował");
+    }
+
+    private static Direction[] convertToDirections( String[] directions ) {
+        return Arrays
+                .stream(directions)
+                .map( direction -> switch (direction) {
+                    case "f" -> Direction.FORWARD;
+                    case "b" -> Direction.BACKWARD;
+                    case "r" -> Direction.RIGHT;
+                    case "l" -> Direction.LEFT;
+                    default -> Direction.NONE;
+                })
+                .toArray(Direction[]::new);
+    }
+    private static void printLine() {
+        out.println("-------------------------");
+    }
+}
